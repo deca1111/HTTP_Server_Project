4bdc304d54dd0b792977e77fe6ea391de7a7f6d8
deca1111 <leovalette1@gmail.com>
2022-03-07 13:23:40 +0100

    . to ->

--- arbre.c
+++ arbre.c
@@ -4,28 +4,28 @@
 Noeud* creerNoeud(){
 
   Noeud* new= (Noeud*) malloc(sizeof(Noeud));
-  new.value = NULL;
-  new.longueur = 0;
-  new.frere = NULL;
-  new.fils = NULL;
+  new->value = NULL;
+  new->longueur = 0;
+  new->frere = NULL;
+  new->fils = NULL;
 
   return new ;
 }
 
 Noeud* creerFils(Noeud* n){
   Noeud* new = creerNoeud();
-  n.fils = new;
+  n->fils = new;
   return new ;
 }
 
 Noeud* creerFrere(Noeud* n){
   Noeud* new = creerNoeud();
-  n.frere = new ;
+  n->frere = new ;
   return new ;
 }
 
 void setNoeud(Noeud* noeud_, char* tag_, char* value_, int longueur_){
-  noeud_.tag = tag_ ;
-  noeud_.value = value_ ;
-  noeud_.longueur= longueur_ ;
+  noeud_->tag = tag_ ;
+  noeud_->value = value_ ;
+  noeud_->longueur= longueur_ ;
 }
