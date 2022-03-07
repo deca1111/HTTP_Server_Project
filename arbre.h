b5ddbdcf80ef531464a2e3a71e1f65b1a92d8189
Routhred <romain.serpollet@gmail.com>
2022-03-07 15:09:19 +0100

    copy fichier autres branch

---
+++ arbre.h
@@ -0,0 +1,32 @@
+#ifndef ARBRE_H
+#define ARBRE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//Contient les fonctions permettant de creer l'arbre ainsi que la structure de noeud
+
+//Structure de noeud contentant le tag, la valeur et la longueur de la valeur
+//Contient aussi des pointeurs vers des noeuds fils et frere
+typedef struct{
+  char* tag;
+
+  char* valeur;
+  int longueur;
+
+  struct Noeud* fils;
+  struct Noeud* frere;
+
+} Noeud;
+
+Noeud* creerNoeud();
+Noeud* creerFils(Noeud* n);
+Noeud* creerFrere(Noeud* n);
+
+void setNoeud(Noeud* noeud_, char* tag_, char* valeur__, int longueur_);
+
+
+
+
+#endif
