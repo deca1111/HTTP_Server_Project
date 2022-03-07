86b4d8fac9f930c4cfd5afe48e67c7278f6a3df7
Routhred <romain.serpollet@gmail.com>
2022-03-07 15:08:36 +0100

    copy fichier autres branch

---
+++ arbre.c
@@ -0,0 +1,31 @@
+#include "arbre.h"
+#include "api.h"
+
+Noeud* creerNoeud(){
+
+  Noeud* nouveau = (Noeud*) malloc(sizeof(Noeud));
+  nouveau->valeur = NULL;
+  nouveau->longueur = 0;
+  nouveau->frere = NULL;
+  nouveau->fils = NULL;
+
+  return nouveau ;
+}
+
+Noeud* creerFils(Noeud* n){
+  Noeud* nouveau = creerNoeud();
+  n->fils = nouveau;
+  return nouveau ;
+}
+
+Noeud* creerFrere(Noeud* n){
+  Noeud* nouveau = creerNoeud();
+  n->frere = nouveau ;
+  return nouveau ;
+}
+
+void setNoeud(Noeud* noeud_, char* tag_, char* valeur_, int longueur_){
+  noeud_->tag = tag_ ;
+  noeud_->value = valeur_ ;
+  noeud_->longueur= longueur_ ;
+}
