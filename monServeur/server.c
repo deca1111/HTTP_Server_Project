501f95e1a6b08740a58815d9c8ea8c2d051ac2dd
Léo vltt <81568081+deca1111@users.noreply.github.com>
2022-04-30 09:43:51 +0200

    ++

--- monServeur/server.c
+++ monServeur/server.c
@@ -19,20 +19,8 @@
 // this will declare internal type used by the parser
 #include "httpparser.h"
 #include "api.h"
+#include "server.h"
 
-
-#define DIR_DATA "./dataServeur"
-#define ERROR_400 "HTTP/1.0 400 Bad Request\r\n\r\n"
-#define ERROR_404 "HTTP/1.0 404 Not Found\r\n\r\n"
-#define ERROR_415 "HTTP/1.0 415 Unsupported Media Type\r\n\r\n"
-#define ERROR_505 "HTTP/1.0 505 HTTP Version Not Supported\r\n\r\n"
-#define REPONSE_STATUS "HTTP/1.0 200 OK\r\n"
-#define REPONSE_CONTENT_TYPE "Content-Type: "
-#define REPONSE_CONTENT_LENGTH "Content-Length: "
-#define REPONSE_DATE "Date: "
-#define SAUT_DE_LIGNE "\r\n"
-#define MAX_SIZE 80
-#define SIZE_MIME 30
 
 char* matrice_type[SIZE_MIME][2]={
 		{"html","text/html\r\n"},
